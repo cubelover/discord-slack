debe9bdc090c40c526dd3993f987d87ff33a2c61
cubelover <jeehak0210@gmail.com>
2020-03-08 09:36:38 +0900

    restart

--- index.js
+++ index.js
@@ -23,70 +23,110 @@ axios.get(`https://slack.com/api/users.list?token=${process.env.STOKEN}`).then((
 });
 
 let slack, discord;
-axios.get(`https://slack.com/api/rtm.connect?token=${process.env.STOKEN}`).then(({ data }) => {
-  console.log('Slack Connected');
-  slack = new WebSocket(data.url);
-  slack.on('message', (data) => {
+
+function slack_start() {
+  axios.get(`https://slack.com/api/rtm.connect?token=${process.env.STOKEN}`).then(({ data }) => {
+    let alive = true, ping;
+    slack = new WebSocket(data.url);
+    slack.on('open', () => {
+      ping = setInterval(() => {
+        if (!alive) {
+          slack.terminate();
+          return;
+        }
+        alive = false;
+        slack.send('{"id":0,"type":"ping"}');
+      }, 10000);
+    });
+    slack.on('message', (data) => {
+      try {
+        let { type, subtype, channel, user, text } = JSON.parse(data);
+        if (type === 'message' && !subtype && channel === process.env.SCHANNEL) {
+          stod.forEach(([u, v]) => {
+            text = text.split(u).join(v);
+          });
+          axios.post(`https://discordapp.com/api/channels/${process.env.DCHANNEL}/messages`, {
+            content: `<${slackname[user]}> ${text}`,
+            tts: false,
+          });
+        }
+        if (type === 'pong') {
+          alive = true;
+          console.log(new Date().toISOString(), 'slack pong');
+        }
+      } catch (err) {
+        console.error(err, data);
+      }
+    });
+    slack.on('close', () => {
+      clearInterval(ping);
+      slack_start();
+    });
+  });
+}
+
+function discord_start() {
+  let last_s, alive = true, ping;
+  discord = new WebSocket('wss://gateway.discord.gg/?v=6&encoding=json');
+  discord.on('message', (data) => {
     try {
-      let { type, subtype, channel, user, text } = JSON.parse(data);
-      if (type === 'message' && !subtype && channel === process.env.SCHANNEL) {
-        stod.forEach(([u, v]) => {
-          text = text.split(u).join(v);
-        });
-        axios.post(`https://discordapp.com/api/channels/${process.env.DCHANNEL}/messages`, {
-          content: `<${slackname[user]}> ${text}`,
-          tts: false,
-        });
+      let { op, d, s, t } = JSON.parse(data);
+      last_s = s;
+      if (op === 0) {
+        if (t === 'MESSAGE_CREATE' && d.channel_id == process.env.DCHANNEL && !d.author.bot) {
+          let text = d.content;
+          dtos.forEach(([u, v]) => {
+            text = text.split(u).join(v);
+          });
+          text = text.replace( /<:([a-z0-9\_]+):[0-9]{18}>/gm, ":$1:");
+          slack.send(JSON.stringify({
+            type: 'message',
+            channel: process.env.SCHANNEL,
+            text: `<${d.member.nick || d.author.username}> ${text}`,
+          }));
+        }
+        if (t === 'GUILD_CREATE') {
+          d.members.forEach(({ user, nick }) => {
+            const name = nick || user.username;
+            dtos.push([`<@!${user.id}>`, `&lt;@!${name}&gt;`]);
+            stod.push([`<@!${name}>`, `<@!${user.id}>`]);
+          });
+        }
+      }
+      if (op === 10) {
+        discord.send(JSON.stringify({
+          op: 2,
+          d: {
+            token: process.env.DTOKEN,
+            properties: {
+              '$os': 'linux',
+              '$browser': 'disco',
+              '$device': 'disco',
+            },
+          },
+        }));
+        ping = setInterval(() => {
+          if (!alive) {
+            discord.terminate();
+            return;
+          }
+          alive = false;
+          discord.send(JSON.stringify({ op: 1, d: s }));
+        }, d.heartbeat_interval);
+      }
+      if (op === 11) {
+        alive = true;
+        console.log(new Date().toISOString(), 'discord pong');
       }
     } catch (err) {
       console.error(err, data);
     }
   });
-  setInterval(() => slack.send('{"id":0,"type":"ping"}'), 10000);
-});
+  discord.on('close', () => {
+    clearInterval(ping);
+    discord_start();
+  });
+}
 
-let last_s;
-discord = new WebSocket('wss://gateway.discord.gg/?v=6&encoding=json');
-discord.on('message', (data) => {
-  try {
-    let { op, d, s, t } = JSON.parse(data);
-    last_s = s;
-    if (op === 0) {
-      if (t === 'MESSAGE_CREATE' && d.channel_id == process.env.DCHANNEL && !d.author.bot) {
-        let text = d.content;
-        dtos.forEach(([u, v]) => {
-          text = text.split(u).join(v);
-        });
-        text = text.replace( /<:([a-z0-9\_]+):[0-9]{18}>/gm, ":$1:");
-        slack.send(JSON.stringify({
-          type: 'message',
-          channel: process.env.SCHANNEL,
-          text: `<${d.member.nick || d.author.username}> ${text}`,
-        }));
-      }
-      if (t === 'GUILD_CREATE') {
-        d.members.forEach(({ user, nick }) => {
-          const name = nick || user.username;
-          dtos.push([`<@!${user.id}>`, `&lt;@!${name}&gt;`]);
-          stod.push([`<@!${name}>`, `<@!${user.id}>`]);
-        });
-      }
-    }
-    if (op === 10) {
-      discord.send(JSON.stringify({
-        op: 2,
-        d: {
-          token: process.env.DTOKEN,
-          properties: {
-            '$os': 'linux',
-            '$browser': 'disco',
-            '$device': 'disco',
-          },
-        },
-      }));
-      setInterval(() => discord.send(JSON.stringify({ op: 1, d: s })), d.heartbeat_interval);
-    }
-  } catch (err) {
-    console.error(err, data);
-  }
-});
+slack_start();
+discord_start();
