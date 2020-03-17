955783584c20a089bfc75d04b10bbd9671a6d065
cubelover <jeehak0210@gmail.com>
2020-03-18 00:38:51 +0900

    remove bluff

--- bot.js
+++ bot.js
@@ -62,53 +62,46 @@ function bluffRound(res) {
 }
 
 const commandHandler = {
-  '!test': (res, who) => DM(res, who, users[who]),
-  '!bluff': (res, who, cmd, ...args) => {
-    if (bluff.state) {
-      if (who != bluff.pp[0][0]) return;
-      if (cmd === 'bluff' && bluff.last) {
-        const [tw, tx, ty] = bluff.last;
-        delete bluff.last;
-        const cnt = Object.values(bluff.dice).reduce((s, e) => e.reduce((s, t) => s + (t === 6 || t === tx), s), 0);
-        CM(res, `${users[who]}의 도전: ${tx}${'이가'[52 >> tx & 1]} ${cnt}개 (차이: ${ty - cnt})\n${bluff.pp.map(([e]) => `${users[e]}: ${bluff.dice[e].join(' ')}`).join('\n')}`);
-        if (ty < cnt) bluff.pp.find(([e]) => e === who)[1] -= cnt - ty;
-        else if (ty > cnt) bluff.pp.find(([e]) => e === tw)[1] -= ty - cnt;
-        else bluff.pp.forEach((e) => {
-          if (e[0] != tw) e[1] -= 1;
-        });
-        bluffRound(res);
-      }
-      if (cmd === 'bet') {
-        const x = +args[0] | 0, y = +args[1] | 0;
-        if (x >= 1 && x <= 6 && y >= 1) {
-          const [, tx, ty] = bluff.last || [0, 0, 0];
-          if ((x === 6 ? y + y - 1 : y) * 10 + x > (tx === 6 ? ty + ty - 1 : ty) * 10 + tx) {
-            bluff.pp.push(bluff.pp.shift());
-            bluff.last = [who, x, y];
-            CM(res, `${users[who]}의 베팅: ${x}${'이가'[52 >> x & 1]} ${y}개, ${users[bluff.pp[0][0]]}의 차례`);
-            return;
-          }
-        }
-      }
-      return;
+  '!join': (res, who) => {
+    if (bluff.state || bluff.pp.includes(who)) return;
+    bluff.pp.push(who);
+    CM(res, `현재 ${bluff.pp.length}명 (${bluff.pp.map(e => users[e]).join(', ')})`);
+  },
+  '!start': (res, who) => {
+    if (bluff.state || !bluff.pp.includes(who)) return;
+    for (let i = 1; i < bluff.pp.length; i += 1) {
+      const j = Math.random() * (i + 1) | 0;
+      const t = bluff.pp[i];
+      bluff.pp[i] = bluff.pp[j];
+      bluff.pp[j] = t;
     }
-    if (cmd === 'join') {
-      if (!bluff.pp.includes(who)) {
-        bluff.pp.push(who);
-        CM(res, `블러프 현재 ${bluff.pp.length}명 (${bluff.pp.map(e => users[e]).join(', ')})`);
-      }
-    }
-    if (cmd === 'start' && bluff.pp.includes(who)) {
-      for (let i = 1; i < bluff.pp.length; i += 1) {
-        const j = Math.random() * (i + 1) | 0;
-        const t = bluff.pp[i];
-        bluff.pp[i] = bluff.pp[j];
-        bluff.pp[j] = t;
-      }
-      bluff.state = 1;
-      bluff.pp = bluff.pp.map(e => [e, 5]);
-      bluffRound(res);
-    }
+    bluff.state = 1;
+    bluff.pp = bluff.pp.map(e => [e, 5]);
+    bluffRound(res);
+  },
+  '!bet': (res, who, ...args) => {
+    if (!bluff.state || who !== bluff.pp[0][0]) return;
+    const x = +args[0] | 0, y = +args[1] | 0;
+    if (x < 1 || x > 6 || y < 1) return;
+    const [, tx, ty] = bluff.last || [0, 0, 0];
+    if ((x === 6 ? y + y - 1 : y) * 10 + x <= (tx === 6 ? ty + ty - 1 : ty) * 10 + tx) return;
+    bluff.pp.push(bluff.pp.shift());
+    bluff.last = [who, x, y];
+    CM(res, `${users[who]}의 베팅: ${x}${'이가'[52 >> x & 1]} ${y}개, ${users[bluff.pp[0][0]]}의 차례`);
+  },
+  '!bluff': (res, who) => {
+    if (!bluff.state || who !== bluff.pp[0][0] || !bluff.last) return;
+    const [tw, tx, ty] = bluff.last;
+    delete bluff.last;
+    const cnt = Object.values(bluff.dice).reduce((s, e) => e.reduce((s, t) => s + (t === 6 || t === tx), s), 0);
+    const text = `${users[who]}의 도전: ${tx}${'이가'[52 >> tx & 1]} ${cnt}개 (차이: ${ty - cnt})\n${bluff.pp.map(([e]) => `${users[e]}: ${bluff.dice[e].join(' ')}`).join('\n')}`;
+    if (ty < cnt) bluff.pp.find(([e]) => e === who)[1] -= cnt - ty;
+    else if (ty > cnt) bluff.pp.find(([e]) => e === tw)[1] -= ty - cnt;
+    else bluff.pp.forEach((e) => {
+      if (e[0] != tw) e[1] -= 1;
+    });
+    bluffRound(res);
+    CM(res, text);
   },
 };
 
